a107841e19544d11676fec05d91a148c55b5aec8
KanyeNord <alessio.scarabotta@iisgubbio.edu.it>
2024-03-26 23:42:57 +0100

    fix movimento rotto

--- Snake.java
+++ Snake.java
@@ -22,11 +22,15 @@ import javafx.util.Duration;
 //schermata di vittoria e sconfitta
 //menù principale e personalizzazione
 //lunghezza serpente
+//collisioni con il serpente
+//sistemare le dimensioni diverse
+//sistemare bug mele multiple
+//effetto pacman
 
 public class Snake extends Application {
-	int campoX=30;
-	int campoY=30;
-	
+	int campoX=15;
+	int campoY=15;
+
 	GridPane griglia = new GridPane();
 	Pane pannello = new Pane();
 	Label eTitolo = new Label("Snake");
@@ -34,54 +38,54 @@ public class Snake extends Application {
 	Boolean mele[][] = new Boolean [campoX][campoY];
 	Boolean serpente[][] = new Boolean [campoX][campoY];
 	Label campo[][] = new Label [campoX][campoY];
-	
+
 	TextField cVelocità = new TextField();
 	TextField cGrandezza = new TextField();
-	
+
 	boolean alto=false;
 	boolean basso=false;
 	boolean sinistra=false;
 	boolean destra=true;
-	
+
 	int snakeX=2;
 	int snakeY=campoY/2;
 
 	int punti=0;
 
-	int larghezza=campoX*30;
-	int lunghezza=campoY*30;
+	int larghezza=campoY*30;
+	int lunghezza=campoX*30;
 	Label ePunteggio = new Label(punti+"");
-	
+
 	Timeline timeline = new Timeline(new KeyFrame(
-			Duration.seconds(1.5),
+			Duration.millis(300),
 			x -> aggiornaTimer()));
 
 	public void start(Stage finestra) {
 		eTitolo.setAlignment(Pos.CENTER);
 		griglia.add(pannello, 0, 0, 15, 15);
-		
+
 		pannello.setPrefSize(larghezza, lunghezza);
 		pannello.getChildren().add(eTitolo);
 		pannello.getChildren().add(bInizia);
 		pannello.getChildren().add(cVelocità);
 		pannello.getChildren().add(cGrandezza);
-		
+
 		eTitolo.setLayoutX(larghezza/2);
 		eTitolo.setLayoutY(30);
-		
+
 		bInizia.setLayoutX(larghezza/2);
 		bInizia.setLayoutY(60);
 
 		cVelocità.setLayoutX(larghezza/2);
 		cVelocità.setLayoutY(90);
-		
+
 		cGrandezza.setLayoutX(larghezza/2);
 		cGrandezza.setLayoutY(120);
-		
+
 		timeline.setCycleCount(Animation.INDEFINITE);
 		timeline.stop();
-		
-		
+
+
 		bInizia.setOnAction(e-> inizia());
 		griglia.setPrefSize(larghezza, lunghezza);
 		griglia.setId("griglia");
@@ -96,15 +100,16 @@ public class Snake extends Application {
 	public void inizia() {
 		bInizia.setVisible(false);
 		eTitolo.setVisible(false);
+		pannello.setVisible(false);
 		timeline.play();
 		//crea il campo e lo colora
 		for(int y=0; y<campo.length;y++) {
 			for(int x=0; x<campo.length;x++) {
-				
+
 				campo[x][y]= new Label();
 				griglia.add(campo[x][y], x+1, y);
 				campo[x][y].setPrefSize(30, 30);
-				
+
 				int colorX=x;
 				if(y%2==0) {
 					colorX+=1;
@@ -116,6 +121,12 @@ public class Snake extends Application {
 				}
 			}
 		}
+		//crea la griglia del serpente
+		for(int y=0; y<serpente.length;y++) {
+			for(int x=0; x<serpente.length;x++) {
+				serpente[x][y]= false;
+			}
+		}
 		//crea la griglia di mele
 		for(int y=0; y<mele.length;y++) {
 			for(int x=0; x<mele.length;x++) {
@@ -125,12 +136,16 @@ public class Snake extends Application {
 		//genera la prima mela
 		mele[campoX-3][campoY/2] = true;
 		campo[campoX-3][campoY/2].getStyleClass().add("mela");
+		//genera la prima mela
+				serpente[2][campoY/2] = true;
+				campo[2][campoY/2].getStyleClass().add("serpente");
 	}
 	private void aggiornaTimer() {
 		//conteggio punti e generazione nuova mela
 
-		int xVecchio=snakeX;
-		int yVecchio=snakeY;
+		int xVecchio=snakeX-punti;
+		int yVecchio=snakeY-punti;
+
 		if (mele[snakeX][snakeY]==true) {
 			int melaX=(int)(Math.random() * campoX-1);
 			int melaY=(int)(Math.random() * campoY-1);
@@ -140,59 +155,71 @@ public class Snake extends Application {
 			punti+=1;
 		}
 		//movimento
-	if (alto && snakeY!=0) {
-		snakeY-=1;
-		serpente[snakeX][snakeY]=serpente[xVecchio][yVecchio];
-		campo[snakeX][snakeY].getStyleClass().add("serpente");
+		if (alto && snakeY!=0) {
+			snakeY-=1;
+			serpente[snakeX][snakeY]=true;
+			serpente[xVecchio][yVecchio]=false;
+		//	campo[snakeX][snakeY].getStyleClass().add("serpente");
+			
 		}
-	if (basso && snakeY!=campoY-1) {
-		snakeY+=1;
-		serpente[snakeX][snakeY]=serpente[xVecchio][yVecchio];
-		campo[snakeX][snakeY].getStyleClass().add("serpente");
+		if (basso && snakeY!=campoY-1) {
+			snakeY+=1;
+			serpente[snakeX][snakeY]=serpente[xVecchio][yVecchio];
+		//	campo[snakeX][snakeY].getStyleClass().add("serpente");
 		}
-	if (sinistra && snakeX!=0) {
-		snakeX-=1;
-		serpente[snakeX][snakeY]=serpente[xVecchio][yVecchio];
-		campo[snakeX][snakeY].getStyleClass().add("serpente");
+		if (sinistra && snakeX!=0) {
+			snakeX-=1;
+			serpente[snakeX][snakeY]=serpente[xVecchio][yVecchio];
+		//	campo[snakeX][snakeY].getStyleClass().add("serpente");
 		}
-	if (destra && snakeX!=campoX-1) {
-		snakeX+=1;
-		serpente[snakeX][snakeY]=serpente[xVecchio][yVecchio];
-		campo[snakeX][snakeY].getStyleClass().add("serpente");
+		if (destra && snakeX!=campoX-1) {
+			snakeX+=1;
+			serpente[snakeX][snakeY]=serpente[xVecchio][yVecchio];
+		//	campo[snakeX][snakeY].getStyleClass().add("serpente");
 		}
+		
+//		for(int y=0; y<serpente.length;y++) {
+//			for(int x=0; x<serpente.length;x++) {
+//				if (serpente[x][y]) {
+//					campo[x][y].getStyleClass().add("serpente");
+//				} else {
+//					campo[x][y].getStyleClass().remove("serpente");
+//				}
+//			}
+//		}
 	}
 	private void pigiato(KeyEvent evento) {
 		if(evento.getText().equals("w") || evento.getText().equals("W") || evento.getCode() == KeyCode.UP) {
-//			if (serpente[][]!=0) {
+			//			if (serpente[][]!=0) {
 			alto=true;
 			basso=false;
 			sinistra=false;
 			destra=false;
-//			}
+			//			}
 		}
 		if(evento.getText().equals("s") || evento.getText().equals("S") || evento.getCode() == KeyCode.DOWN) {
-//			if (serpente[][]!=15) {
+			//			if (serpente[][]!=15) {
 			alto=false;
 			basso=true;
 			sinistra=false;
 			destra=false;
-//			}
+			//			}
 		}
 		if(evento.getText().equals("a") || evento.getText().equals("A") || evento.getCode() == KeyCode.LEFT) {
-//			if (serpente[][]!=0) {
+			//			if (serpente[][]!=0) {
 			alto=false;
 			basso=false;
 			sinistra=true;
 			destra=false;
-//			}
+			//			}
 		}
 		if(evento.getText().equals("d") || evento.getText().equals("D") || evento.getCode() == KeyCode.RIGHT) {
-//			if (serpente[][]!=15) {
+			//			if (serpente[][]!=15) {
 			alto=false;
 			basso=false;
 			sinistra=false;
 			destra=true;
-//			}
+			//			}
 		}
 	}
 	public static void main(String[] args) {
